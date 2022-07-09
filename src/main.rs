1ffc02922772ed9643c0c64acf65334278238e97
Chris Petkau <chris.petkau@kleientertainment.com>
2022-07-09 16:56:03 -0700

    Implemented find_zip().

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-fn main() {
-    // Import folder is hard-coded to "C:\Users\Chris Petkau\Downloads".
+use anyhow::{anyhow, Result};
+use std::fs;
+
+// Import folder is hard-coded to "C:\Users\Chris Petkau\Downloads".
+const IMPORT_FOLDER: &str = "C:\\Users\\Chris Petkau\\Downloads";
+
+// Export folder is hard-coded to "C:\src\qmk_firmware\keyboards\moonlander\keymaps\chrispetkau".
+const EXPORT_FOLDER: &str = "C:\\src\\qmk_firmware\\keyboards\\moonlander\\keymaps\\chrispetkau";
+
+fn main() -> Result<()> {
     // Find the most recent downloaded file with prefix "moonlander_" and extension ".zip".
+    let zip = find_zip()?;
 
     // Manifest a "temp" folder.
 
     // Extract files and put them in the "temp" folder.
-
-    // Export folder is hard-coded to "C:\src\qmk_firmware\keyboards\moonlander\keymaps\chrispetkau".
 
     // Update "config.h" via "temp\config.h": copy every line, then #include "petkau_config.inl".
 
@@ -20,8 +27,34 @@ fn main() {
 
     // Stage and commit all changes via git.
 
-    // Invoke "C:\Program Files (x86)\Wally\Wally.exe" to flash. 
+    // Invoke "C:\Program Files (x86)\Wally\Wally.exe" to flash.
     // Filename is "C:\src\qmk_firmware\moonlander_chrispetkau.bin".
+
+    Ok(())
+}
+
+fn find_zip() -> Result<String> {
+    Ok(fs::read_dir(IMPORT_FOLDER)?
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let file_name = entry.file_name().into_string().ok()?;
+            let time_stamp = entry.metadata().ok()?.modified().ok()?;
+            if entry.file_type().ok()?.is_file() {
+                Some((file_name, time_stamp))
+            } else {
+                None
+            }
+        })
+        .filter_map(|(file_name, time_stamp)| {
+            if file_name.starts_with("moonlander_") && file_name.ends_with(".zip") {
+                Some((file_name, time_stamp))
+            } else {
+                None
+            }
+        })
+        .max_by_key(|(_, time_stamp)| *time_stamp)
+        .ok_or_else(|| anyhow!("No .zip file found."))?
+        .0)
 }
 
 fn update_keymap_c() {
@@ -38,15 +71,15 @@ fn update_keymap_c() {
     // Overwrite petkau_tap_dance.inl with tap_dance_defs.
 
     // Map macros by parsing the arguments of the SEND_STRING calls in macro_defs and converting them into strings.
-    // These strings are then matched against the strings known to correspond to hard-coded macros. Strings 
+    // These strings are then matched against the strings known to correspond to hard-coded macros. Strings
     // extracted from the macro_defs may not be complete and so partial matches must be considered. Multiple matches
     // may occur and constitute an error: strings in macro_defs must match only a single macro in petkau_macros.inl.
     // This produces an array whose indices map to ST_MACRO_# and contents map to the corresponding enum value of the
     // hard-coded macro enum in petkau_macros.inl.
 
-    // Create custom_keymap by replacing all occurrences of "ST_MACRO_#" in keymap with the corresponding entry in the 
+    // Create custom_keymap by replacing all occurrences of "ST_MACRO_#" in keymap with the corresponding entry in the
     // macro map.
-    
+
     // The new keymap.c exports like this:
     // - preprocessing
     // - #include "petkau_macros.inl"
